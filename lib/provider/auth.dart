bf8e62131442193b786aa1ee39401abc33a4849e
Vijayasekhar Deepak <Vijayasekhar.Deepak@walmart.com>
2021-07-04 08:23:25 +0530

    update

--- lib/provider/auth.dart
+++ lib/provider/auth.dart
@@ -39,7 +39,7 @@ class Auth with ChangeNotifier {
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCdZqv-X4DlVxeE1A0sluz129B-dra98Vc');
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=');
     try {
       final res = await http.post(url,
           body: json.encode({
